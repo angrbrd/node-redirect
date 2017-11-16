3010accb69f20966a529489e4e26eeb3904c65ff
Anna D Derbakova <hiopa16@gmail.com>
2017-11-16 15:06:30 -0500

    Adding POST processing for /destination URL
    POST /destination

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 // Dependencies
 // ===========================================================
 var express = require("express");
+var bodyParser = require("body-parser");
 
+// Configure the Express application
+// ===========================================================
 var app = express();
 var PORT = 8080;
+
+// Set up the Express application to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Routes
 // ===========================================================
 app.get("/home", function(req, res) {
-  console.log("___ENTER /home___");
+  console.log("___ENTER GET /home___");
 
   res.send("Welcome home!");
 });
 
 app.get("/redirect", function(req, res) {
-  console.log("___ENTER /redirect___");
+  console.log("___ENTER GET /redirect___");
 
   res.writeHead(302, {
     'Location': '/destination'
@@ -24,9 +31,23 @@ app.get("/redirect", function(req, res) {
 });
 
 app.get("/destination", function(req, res) {
-  console.log("___ENTER /destination___");
+  console.log("___ENTER GET /destination___");
 
   res.send("Destination page.");
+});
+
+app.post("/destination", function(req, res) {
+  console.log("___ENTER POST /destination___");
+
+  var reqPayload = req.body;
+  console.log("Received reqPayload = " + JSON.stringify(reqPayload, null, 4));
+
+  // Add some additional fields to the response payload
+  var resPayload = reqPayload;
+  reqPayload.name = "Anya";
+  reqPayload.type = "awesome";
+
+  res.json(resPayload);
 });
 
 // Listener
